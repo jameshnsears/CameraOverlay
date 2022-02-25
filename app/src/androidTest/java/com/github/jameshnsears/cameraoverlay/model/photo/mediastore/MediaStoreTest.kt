560d891ce11612cfa38752312917810eab72e2d3
James HN Sears <james.hn.sears@gmail.com>
2022-02-25 18:56:47 +0200

    mediastore - wip

--- app/src/androidTest/java/com/github/jameshnsears/cameraoverlay/model/photo/mediastore/MediaStoreTest.kt
+++ app/src/androidTest/java/com/github/jameshnsears/cameraoverlay/model/photo/mediastore/MediaStoreTest.kt
@@ -1,35 +1,36 @@
 package com.github.jameshnsears.cameraoverlay.model.photo.mediastore
 
 import android.Manifest
+import android.app.Application
 import android.content.ContentUris
 import android.content.Context
 import android.net.Uri
-import android.os.Build
 import android.provider.MediaStore
-import android.util.Log
-import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.platform.app.InstrumentationRegistry
-import junit.framework.Assert.assertEquals
-import junit.framework.Assert.fail
-import org.junit.Test
 import androidx.test.rule.GrantPermissionRule
+import java.net.URLConnection
+import junit.framework.Assert.assertEquals
 import org.junit.Rule
-import org.junit.runner.RunWith
-import timber.log.Timber
+import org.junit.Test
+import android.content.ContentResolver
+
+
+
 
 
 class MediaStoreTest {
     private val context: Context = InstrumentationRegistry.getInstrumentation().targetContext
 
     @get:Rule
-    var mRuntimePermissionRule = GrantPermissionRule.grant(
-        Manifest.permission.READ_EXTERNAL_STORAGE,
+    var permissionRule = GrantPermissionRule.grant(
+        Manifest.permission.READ_EXTERNAL_STORAGE,  // access other apps files, outside of Scoped Storage
         Manifest.permission.ACCESS_MEDIA_LOCATION   // retrieve unredacted Exif metadata from photos
     )
 
     @Test
     fun mediaStoreTest() {
-//        val application = ApplicationProvider.getApplicationContext() as Application
+        val application = ApplicationProvider.getApplicationContext() as Application
 //        val viewModel = MainActivityViewModel(application)
 //        viewModel.loadImages()
 
@@ -48,31 +49,97 @@ class MediaStoreTest {
 
         val galleryImageUrls = mutableListOf<Uri>()
 
-        context.contentResolver.query(
-            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-            arrayOf(
-                MediaStore.Images.Media._ID,
-                MediaStore.Images.Media.DISPLAY_NAME,
-                MediaStore.Images.Media.RELATIVE_PATH
-            ),
+        // "external" == actually internal on the Phone!
+        val externalContentUri = MediaStore.Files.getContentUri("external")
+            ?: throw Exception("External Storage not available")
+
+        val projection = arrayOf(
+            MediaStore.Files.FileColumns._ID,
+            MediaStore.Files.FileColumns.DISPLAY_NAME,
+            MediaStore.Files.FileColumns.SIZE,
+            MediaStore.Files.FileColumns.MEDIA_TYPE,
+            MediaStore.Files.FileColumns.MIME_TYPE,
+            MediaStore.Files.FileColumns.DATA,
+        )
+
+        val cursor = context.contentResolver.query(
+            externalContentUri,
+            projection,
             null,
             null,
-            null    // "${MediaStore.Images.Media.DATE_ADDED} DESC"
-        )?.use { cursor ->
-            val idColumn = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+            "${MediaStore.MediaColumns.DATE_ADDED} DESC"
+        ) ?: throw Exception("Query could not be executed")
 
-            Timber.d("Found ${cursor.count} images")
 
+        cursor.use {
             while (cursor.moveToNext()) {
-                val id = cursor.getLong(idColumn)
+                val idColumn = cursor.getColumnIndexOrThrow(MediaStore.Files.FileColumns._ID)
+                val displayNameColumn =
+                    cursor.getColumnIndexOrThrow(MediaStore.Files.FileColumns.DISPLAY_NAME)
+                val sizeColumn = cursor.getColumnIndexOrThrow(MediaStore.Files.FileColumns.SIZE)
+                val mediaTypeColumn =
+                    cursor.getColumnIndexOrThrow(MediaStore.Files.FileColumns.MEDIA_TYPE)
+                val mimeTypeColumn =
+                    cursor.getColumnIndexOrThrow(MediaStore.Files.FileColumns.MIME_TYPE)
+                val dataColumn = cursor.getColumnIndexOrThrow(MediaStore.Files.FileColumns.DATA)
 
-                galleryImageUrls.add(
-                    ContentUris.withAppendedId(
-                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                        id
-                    )
+                val id = cursor.getInt(idColumn)
+                val contentUri: Uri = ContentUris.withAppendedId(
+                    externalContentUri,
+                    id.toLong()
                 )
+
+                val type = context.contentResolver.getType(contentUri)
+
+
+                galleryImageUrls.add(contentUri)
             }
+
+
+            /*
+            context.contentResolver.query(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                arrayOf(
+                    MediaStore.Images.Media._ID,
+                    MediaStore.Images.Media.DISPLAY_NAME,
+                    MediaStore.Images.Media.DATE_ADDED
+                ),
+                null,
+                null,
+                "${MediaStore.Images.Media.DATE_ADDED} DESC"
+            )?.use { cursor ->
+                Timber.d("Found ${cursor.count} images")
+
+                val idColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media._ID)
+                val dateModifiedColumn =
+                    cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATE_ADDED)
+                val displayNameColumn =
+                    cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DISPLAY_NAME)
+
+                while (cursor.moveToNext()) {
+                    val id = cursor.getLong(idColumn)
+
+    //                var photoUri: Uri = Uri.withAppendedPath(
+    //                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+    //                    cursor.getString(idColumn)
+    //                )
+    //
+    //                photoUri = MediaStore.setRequireOriginal(photoUri)
+    //                context.contentResolver.openInputStream(photoUri)?.use { stream ->
+    //                    ExifInterface(stream).run {
+    //                        // If lat/long is null, fall back to the coordinates (0, 0).
+    //                        val latLong = latLong ?: doubleArrayOf(0.0, 0.0)
+    //                    }
+    //                }
+
+                    galleryImageUrls.add(
+                        ContentUris.withAppendedId(
+                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                            id
+                        )
+                    )
+                }
+             */
         }
 
         assertEquals(1, galleryImageUrls.size);
