1b21ed74e60fea58de7605105457fa0edb78eed1
James HN Sears <james.hn.sears@gmail.com>
2022-03-04 17:45:47 +0200

    coil

--- app/src/androidTest/java/com/github/jameshnsears/cameraoverlay/model/photo/coil/CoilTest.kt
+++ app/src/androidTest/java/com/github/jameshnsears/cameraoverlay/model/photo/coil/CoilTest.kt
@@ -5,9 +5,8 @@ import coil.ImageLoader
 import coil.request.ImageRequest
 import com.github.jameshnsears.cameraoverlay.model.photo.PhotoResourcesUtility
 import com.github.jameshnsears.cameraoverlay.model.photo.mediastore.MediaStoreMediator
-import io.mockk.mockk
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertNotNull
 import org.junit.Test
 
 class CoilTest : PhotoResourcesUtility() {
@@ -28,10 +27,9 @@ class CoilTest : PhotoResourcesUtility() {
                 .target(imageView)
                 .build()
 
-            // Executes the ImageRequest in the current coroutine and returns an ImageResult
-            launch {
-                val result = imageLoader.execute(request)
-            }
+            val result = imageLoader.enqueue(request)
+
+            assertNotNull(result)
         }
     }
 }
