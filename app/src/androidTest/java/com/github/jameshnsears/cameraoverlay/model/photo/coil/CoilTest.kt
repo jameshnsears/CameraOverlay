274a83b9d68b617061424fa2980374ebdad80fed
James HN Sears <james.hn.sears@gmail.com>
2022-03-06 16:45:58 +0200

    bumblebee

--- app/src/androidTest/java/com/github/jameshnsears/cameraoverlay/model/photo/coil/CoilTest.kt
+++ app/src/androidTest/java/com/github/jameshnsears/cameraoverlay/model/photo/coil/CoilTest.kt
@@ -2,34 +2,46 @@ package com.github.jameshnsears.cameraoverlay.model.photo.coil
 
 import android.widget.ImageView
 import coil.ImageLoader
+import coil.load
 import coil.request.ImageRequest
+import com.github.jameshnsears.cameraoverlay.R
 import com.github.jameshnsears.cameraoverlay.model.photo.PhotoResourcesUtility
 import com.github.jameshnsears.cameraoverlay.model.photo.mediastore.MediaStoreMediator
-import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertNotNull
 import org.junit.Test
 
 class CoilTest : PhotoResourcesUtility() {
     @Test
     fun coil() {
-        runBlocking {
-            val picturesInMediaStore = MediaStoreMediator.picturesInMediaStore(context)
+        val picturesInMediaStore = MediaStoreMediator.picturesInMediaStore(context)
 
-            val imageLoader = ImageLoader.Builder(context)
-                .availableMemoryPercentage(0.25)
-                .crossfade(true)
-                .build()
+        // https://github.com/coil-kt/coil
 
-            var imageView = ImageView(context)
+        // .kt extension function on ImageView
+        var imageView = ImageView(context)
+        // TODO use a url not uri
+        imageView.load(picturesInMediaStore[0].uri)
+        assertNotNull(imageView)
 
-            val request = ImageRequest.Builder(context)
-                .data(picturesInMediaStore[0].uri)
-                .target(imageView)
-                .build()
+        //////////////////
 
-            val result = imageLoader.enqueue(request)
-
-            assertNotNull(result)
+        // https://github.com/coil-kt/coil/issues/559
+        // https://coil-kt.github.io/coil/image_pipeline/#fetchers
+        imageView.load(picturesInMediaStore[0].uri) { // content://media/external/video/media/6945
+//            fetcher(VideoFrameUriFetcher(context))
         }
+
+        /*
+        Preload:
+        val image = Coil.get(imageUrl)
+
+        imageView.load(File("/path/to/ironman.png"))
+
+        transformations(CircleCropTransformation())
+        transformations(RoundedCornersTransformation(25f))
+        transformations(GrayscaleTransformation())
+        transformations(BlurTransformation(applicationContext))
+        transformations(BlurTransformation(applicationContext, 5f))
+         */
     }
 }
