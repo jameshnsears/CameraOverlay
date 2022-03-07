73045bd299979f7879fe131e8dfce629917c3247
James HN Sears <james.hn.sears@gmail.com>
2022-03-07 16:56:56 +0200

    rm picasso

--- app/src/androidTest/java/com/github/jameshnsears/cameraoverlay/model/photo/coil/PhotosTest.kt
+++ app/src/androidTest/java/com/github/jameshnsears/cameraoverlay/model/photo/coil/PhotosTest.kt
@@ -1,14 +1,11 @@
 package com.github.jameshnsears.cameraoverlay.model.photo.coil
 
-import android.graphics.drawable.BitmapDrawable
-import android.os.Handler
-import android.os.Looper
+
 import android.widget.ImageView
 import coil.ImageLoader
 import coil.request.ImageRequest
 import com.github.jameshnsears.cameraoverlay.model.photo.PhotoResourcesUtility
 import com.github.jameshnsears.cameraoverlay.model.photo.mediastore.MediaStoreMediator
-import com.squareup.picasso.Picasso
 import org.junit.Assert.assertNotNull
 import org.junit.Test
 
@@ -52,31 +49,5 @@ class PhotosTest : PhotoResourcesUtility() {
         transformations(BlurTransformation(applicationContext))
         transformations(BlurTransformation(applicationContext, 5f))
          */
-    }
-
-    @Test
-    fun picasso() {
-        val picturesInMediaStore = MediaStoreMediator.picturesInMediaStore(context)
-
-        var imageView = ImageView(context)
-
-        // https://developer.android.com/guide/background
-
-        // TODO https://github.com/diffplug/spotless/tree/main/plugin-gradle
-
-        // TODO put mediastore db calls in a coroutine, as db calls expensive
-
-        // Method call should happen from the main thread.
-        Handler(Looper.getMainLooper()).post {
-//            Picasso.get().load(picturesInMediaStore[0].uri).into(imageView)
-              Picasso.get().load("https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png").into(imageView)
-
-            val bd = imageView.drawable as BitmapDrawable
-            val b = bd.bitmap
-        }
-
-
-
-        assertNotNull(imageView)
     }
 }
