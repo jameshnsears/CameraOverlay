d7643197a97a3911531657744c99195e8cdb1649
James HN Sears <james.hn.sears@gmail.com>
2022-03-17 09:50:00 +0200

    Card / Coil - wip

--- app/src/main/java/com/github/jameshnsears/cameraoverlay/view/photo/PhotoCard.kt
+++ app/src/main/java/com/github/jameshnsears/cameraoverlay/view/photo/PhotoCard.kt
@@ -1,22 +1,17 @@
 package com.github.jameshnsears.cameraoverlay.view.photo
 
-import android.graphics.BitmapFactory
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.painter.BitmapPainter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -24,16 +19,11 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
-import coil.compose.rememberImagePainter
-import coil.request.ImageRequest
 import com.github.jameshnsears.cameraoverlay.R
 
 @Preview
 @Composable
 fun CardPhoto() {
-
-    val context = LocalContext.current
-
     Card(
         elevation = 8.dp,
         modifier = Modifier
@@ -65,26 +55,28 @@ v.
 https://material.io/components/cards
  */
 
-                Column() {
+                Column {
                     // placeholder provides the @Preview image
                     AsyncImage(
                         model = "https://example.com/image.jpg",
                         placeholder = painterResource(R.drawable.ic_github_logo),
                         contentDescription = "contentDescription",
-                        modifier = Modifier.clip(CircleShape).size(120.dp),
+                        modifier = Modifier
+                            .clip(CircleShape)
+                            .size(120.dp),
                         contentScale = ContentScale.Crop,
                     )
                 }
 
-                Column {
+                Column(Modifier.padding(start = 16.dp)) {
                     Row {
                         Text("Date Taken")
                     }
-                    Row {
+                    Row(Modifier.padding(top = 8.dp, bottom = 8.dp)) {
                         Text("GPS co-ords")
                     }
                     Row {
-                        Text("Path?", fontSize = 10.sp)
+                        Text("Path?", fontSize = 12.sp)
                     }
                 }
             }
