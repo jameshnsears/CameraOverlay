ddb0f468ce9c6e1ace79877ef81abddbd1c7db3c
James HN Sears <james.hn.sears@gmail.com>
2023-07-21 15:41:59 +0100

    fixes to androidTest

--- app/src/androidTest/java/com/github/jameshnsears/cameraoverlay/view/MainScreenTest.kt
+++ app/src/androidTest/java/com/github/jameshnsears/cameraoverlay/view/MainScreenTest.kt
@@ -1,14 +1,15 @@
 package com.github.jameshnsears.cameraoverlay.view
 
+import android.Manifest
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performScrollTo
+import androidx.test.rule.GrantPermissionRule
 import com.github.jameshnsears.cameraoverlay.BuildConfig
 import com.github.jameshnsears.cameraoverlay.R
 import com.github.jameshnsears.cameraoverlay.utility.ScreenshotTestUtility
-import junit.framework.AssertionFailedError
 import junit.framework.TestCase.assertTrue
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -20,47 +21,50 @@ class MainScreenTest : ScreenshotTestUtility() {
     @get:Rule
     val composeTestRule = createAndroidComposeRule<MainActivity>()
 
+    @get:Rule
+    val permissionRule: GrantPermissionRule = GrantPermissionRule.grant(
+        Manifest.permission.READ_EXTERNAL_STORAGE,
+        Manifest.permission.ACCESS_MEDIA_LOCATION,
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+        Manifest.permission.SYSTEM_ALERT_WINDOW
+    )
+
     @Test
     fun mainScreen() = runTest {
-        var actualScreenshotFilename = "mainScreen.bmp"
+        var expectedScreenshot = "MainScreenTest.mainScreen.bmp"
 
         launch(Dispatchers.IO) {
             composeTestRule.onNodeWithText(
                 context.resources.getString(R.string.select_photo)
             ).performScrollTo()
 
-            testScreenshot(actualScreenshotFilename,
-                "View/$actualScreenshotFilename"
+            testScreenshot(
+                "View/$expectedScreenshot"
             )
         }.join()
     }
 
     @Test
     fun permissionScreen() = runTest {
-        var actualScreenshotFilename = "permissionScreen.bmp"
+        var expectedScreenshot = "MainScreenTest.permissionScreen.bmp"
 
         launch(Dispatchers.IO) {
             composeTestRule.onNodeWithContentDescription(
                 "?"
             ).performClick()
 
-            testScreenshot(actualScreenshotFilename,
-                "View/$actualScreenshotFilename"
+            testScreenshot(
+                "View/$expectedScreenshot"
             )
         }.join()
     }
 
     private fun testScreenshot(
-        actualScreenshotFilename: String,
-        expectedScreenshot: String) {
+        expectedScreenshot: String
+    ) {
         if (!BuildConfig.GITHUB_ACTION) {
             takeScreenshot(composeTestRule)
-
-            // just in case there is a problem, to aid manual comparison
-            saveScreenshotToInternalStorage(
-                "actual-$actualScreenshotFilename",
-                getActualScreenshot()!!
-            )
 
             assertTrue(
                 getActualScreenshot()!!.sameAs(
